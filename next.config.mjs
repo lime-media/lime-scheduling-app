50954c20114a8c22a172adc4958063c94c491782
SarahLime <schaudhari@lime-media.com>
2026-04-20 15:24:02 -0500

    Fix Vercel build - Prisma and mssql config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['mssql', 'tedious'],
+  },
+  webpack: (config) => {
+    config.externals.push({
+      'mssql':    'commonjs mssql',
+      'tedious':  'commonjs tedious',
+    })
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
